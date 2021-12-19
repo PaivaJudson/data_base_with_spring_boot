d8af047171568ae6bd9058322e84ee3dfb6dcf2e
Judson Paiva <judson.paiva@hotmail.com>
2021-12-19 08:52:34 +0100

    Primeiro commit

---
+++ src/main/java/com/example/data_base_with_spring_boot/DataBaseWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_base_with_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBaseWithSpringBootApplication.class, args);
+    }
+
+}
